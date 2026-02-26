b094023363edc08e2e6d36a508af7b1df854defa
kaioxavierz <dm.xavierkaio@gmail.com>
2026-02-25 23:33:49 -0300

    config node e prisma

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,4 +1,4 @@
-//import { prisma } from "../../prisma/client";
+import { prisma } from "../client";
 import { IUserController, IUserService, IUserInterface } from "../interfaces/User";
 import { UserService } from "../services/userService";
 
---
+++ src/interfaces/Product.ts
@@ -0,0 +1,6 @@
+export interface IUProductInterface {
+    name: string;
+    description: string;
+    price: number;
+    stock: number;    
+}
