dab4a82a8126d842bd04c0bdb6eec8837295d4cd
kaioxavierz <dm.xavierkaio@gmail.com>
2026-03-03 23:30:47 -0300

    ajustes produtos

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { AppError } from "../utils/AppError";
+import { AppError } from "../utils/appError";
 
 
 interface TokenPayload {
